4123343afcc8e12f72f50fcf6f6f6ba169ea50bd
White <Monsieur_White@hotmail.com>
2014-01-06 20:38:35 +1030

    Initial commit

---
+++ EventStopper/eConfig.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using Newtonsoft.Json;
+
+namespace EventStopper
+{
+    public class eConfig
+    {
+        public bool disableFullMoon = false;
+        public bool disableSnowMoon = false;
+        public bool disableBloodMoon = false;
+        public bool disablePumpkinMoon = false;
+        public bool disableEclipse = false;
+        public bool disableRain = false;
+        public bool disableGoblinInvasion = false;
+        public bool disablePirateInvasion = false;
+        public bool disableFrostLegion = false;
+
+        public static eConfig Read(string path)
+        {
+            if (!File.Exists(path))
+                return new eConfig();
+            using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                return Read(fs);
+            }
+        }
+
+        public static eConfig Read(Stream stream)
+        {
+            using (var sr = new StreamReader(stream))
+            {
+                var cf = JsonConvert.DeserializeObject<eConfig>(sr.ReadToEnd());
+                if (ConfigRead != null)
+                    ConfigRead(cf);
+                return cf;
+            }
+        }
+
+        public void Write(string path)
+        {
+            using (var fs = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.Write))
+            {
+                Write(fs);
+            }
+        }
+        public void Write(Stream stream)
+        {
+            var str = JsonConvert.SerializeObject(this, Formatting.Indented);
+            using (var sw = new StreamWriter(stream))
+            {
+                sw.Write(str);
+            }
+        }
+
+        public static Action<eConfig> ConfigRead;
+    }
+}
---
+++ EventStopper/eStopper.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using TShockAPI;
+
+using Terraria;
+using TerrariaApi;
+using TerrariaApi.Server;
+
+namespace EventStopper
+{
+    [ApiVersion(1,14)]
+    public class eStopper : TerrariaPlugin
+    {
+        public static eConfig config { get; set; }
+        public static string configPath { get { return Path.Combine(TShock.SavePath, "EventStop.json"); } }
+
+        public override string Author { get { return "WhiteX"; } }
+        public override string Description { get { return "Stops config defined events when they start"; } }
+        public override string Name { get { return "EventStopper"; } }
+        public override Version Version { get { return new Version(1, 0); } }
+
+        public eStopper(Main game)
+            : base(game)
+        {
+            Order = 1;
+            config = new eConfig();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                ServerApi.Hooks.GameInitialize.Deregister(this, onInitialize);
+                ServerApi.Hooks.GameUpdate.Deregister(this, onUpdate);
+            }
+            base.Dispose(disposing);
+        }
+
+        public override void Initialize()
+        {
+            ServerApi.Hooks.GameInitialize.Register(this, onInitialize);
+            ServerApi.Hooks.GameUpdate.Register(this, onUpdate);
+        }
+
+        public void onInitialize(EventArgs args)
+        {
+            Commands.ChatCommands.Add(new Command("estopper.reload", reloadCom, "ereload")
+            {
+                HelpText = "Reloads the event stopper plugin's configuration file"
+            });
+            setUpConfig();
+        }
+
+        private void reloadCom(CommandArgs args)
+        {
+            args.Player.SendInfoMessage("Requesting configuration reload");
+            setUpConfig(args.Player);
+        }
+
+        private void onUpdate(EventArgs args)
+        {
+            if (Main.moonPhase == 0 && config.disableFullMoon)
+                TSServerPlayer.Server.SetFullMoon(false);
+
+            if (Main.bloodMoon && config.disableBloodMoon)
+                TSServerPlayer.Server.SetBloodMoon(false);
+
+            if (Main.snowMoon && config.disableSnowMoon)
+                TSServerPlayer.Server.SetSnowMoon(false);
+
+            if (Main.pumpkinMoon && config.disablePumpkinMoon)
+                TSServerPlayer.Server.SetPumpkinMoon(false);
+
+            if (Main.eclipse && config.disableEclipse)
+                TSServerPlayer.Server.SetEclipse(false);
+
+            if (Main.raining && config.disableRain)
+                Main.raining = false;
+
+            if (Main.invasionType > 0)
+                switch (Main.invasionType)
+                {
+                    case 1:
+                        {
+                            if (config.disableGoblinInvasion)
+                            {
+                                Main.invasionType = 0;
+                                Main.invasionSize = 0;
+                            }
+                            break;
+                        }
+                    case 2:
+                        {
+                            if (config.disableFrostLegion)
+                            {
+                                Main.invasionType = 0;
+                                Main.invasionSize = 0;
+                            }
+                            break;
+                        }
+                    case 3:
+                        {
+                            if (config.disablePirateInvasion)
+                            {
+                                Main.invasionType = 0;
+                                Main.invasionSize = 0;
+                            }
+                            break;
+                        }
+                }
+        }
+
+        private void setUpConfig(TSPlayer player = null)
+        {
+            try
+            {
+                if (File.Exists(configPath))
+                    config = eConfig.Read(configPath);
+
+                else
+                    config.Write(configPath);
+
+                if (player != null)
+                    player.SendSuccessMessage("Reloaded event stopper plugin's configuration");
+            }
+            catch (Exception x)
+            {
+                Log.ConsoleError("Error occured on reloading event stopper plugin's configuration");
+                Log.ConsoleError(x.ToString());
+                player.SendErrorMessage("Error occured on reloading event stopper plugin's configuration");
+                player.SendErrorMessage(x.Message);
+            }
+        }
+    }
+}
